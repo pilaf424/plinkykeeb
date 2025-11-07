09211aaf51716fae00c7b90cf6257418846fc49d
Phillip Lopez <philliplopez424@gmail.com>
2025-11-06 22:45:46 -0500

    Add kb2040_groovebox_ui KB2040 project

---
+++ arduino/kb2040_groovebox_ui/kb2040_groovebox_ui.ino
@@ -0,0 +1,10 @@
+// kb2040_groovebox_ui.ino
+#include <Arduino.h>
+
+void setup() {
+  // later: setup USB MIDI, keys, screen, etc.
+}
+
+void loop() {
+  // later: read inputs, draw UI, send MIDI to Daisy.
+}
